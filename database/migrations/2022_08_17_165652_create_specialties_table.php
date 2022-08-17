6df5a74ebc4e1c36993f8aab84a2346b3dd649d7
Joan Roy Carlos Vasquez <joanroycar@gmail.com>
2022-08-17 12:06:15 -0500

    Creación de campos de la tabla especialidades

--- database/migrations/2022_08_17_165652_create_specialties_table.php
+++ database/migrations/2022_08_17_165652_create_specialties_table.php
@@ -15,6 +15,8 @@ class CreateSpecialtiesTable extends Migration
     {
         Schema::create('specialties', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
