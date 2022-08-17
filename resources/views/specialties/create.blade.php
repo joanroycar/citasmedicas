694a05d60bfc6b19c10a9d714d2509e8856175a9
Joan Roy Carlos Vasquez <joanroycar@gmail.com>
2022-08-17 14:40:09 -0500

    Validaciones en vista especialidades

--- resources/views/specialties/create.blade.php
+++ resources/views/specialties/create.blade.php
@@ -17,17 +17,33 @@
         </div>
 
         <div class=" card-body">
+
+            @if ($errors->any())
+
+               @foreach ($errors->all() as $error)
+
+                  <div class="alert alert-danger" role="alert">
+                    <i class="fas fa-exclamation-triangle"></i>
+                    <strong>Por favor!!</strong> {{$error}}
+                  </div>
+                   
+               @endforeach
+                
+            @endif
+
+
+
             <form action="{{url('especialidades')}}" method="POST">
                 @csrf
                 <div class="form-group">
                     <label for="name">Nombre de la especialidad</label>
-                     <input type="text" name="name" class="form-control">
+                     <input type="text" name="name" class="form-control" value="{{old('name')}}">
 
 
                 </div>
                 <div class="form-group">
                     <label for="description">Descripcion</label>
-                     <input type="text" name="description" class="form-control">
+                     <input type="text" name="description" class="form-control" value="{{old('description')}}">
 
 
                 </div>
